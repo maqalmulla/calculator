3362e3c9de21186ef835a999435142c8c459a137
maqalmulla <maqalmulla007@gmail.com>
2022-08-23 09:40:34 +0400

    Added Functionality

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let multiply = (a,b) => {
 }
 
 let divide = (a, b) => {
+    if(b == 0){
+        alert('Naughty..Dont do that')
+    }
     return a / b
 }
 
@@ -15,6 +18,8 @@ let subtract = (a, b) => {
 }
 
 let operate = (a, b, operator) => {
+    a = parseInt(a)
+    b = parseInt(b)
     if(operator == '+'){
         let result = add(a, b);
         return result;
@@ -34,6 +39,7 @@ let num1 = []
 let num2 = []
 let sign = ''
 
+let zero = document.querySelector('.zero button')
 let one = document.querySelector('.one button')
 let two = document.querySelector('.two button')
 let three = document.querySelector('.three button')
@@ -46,155 +52,98 @@ let nine = document.querySelector('.nine button')
 let plus = document.querySelector('.plus button')
 let minus = document.querySelector('.minus button')
 let product = document.querySelector('.multiplu button')
-let division = document.querySelector('.division button')
+let division = document.querySelector('.divide button')
 let operator = document.querySelector('.equal button')
 let result = document.querySelector('.result')
+let clear = document.querySelector('.AC')
+
+let insert = (num) =>{
+    if(sign == ''){
+        num1.push(num)
+        console.log(num1)
+        result.innerHTML = result.innerHTML + num
+    }else{
+        num2.push(num)
+        console.log(num2)
+        result.innerHTML = result.innerHTML + num
+    }
+}
+
+let operation = (signOperator) => {
+    if(sign != ''){
+        alert('Sign is already selected!'); 
+        return
+    } 
+    sign = signOperator
+    console.log('sign')
+    result.innerHTML = ' ' + result.innerHTML + signOperator + ' ' 
+}
 
 one.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(1)
 })
 
 two.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(2)
 })
 three.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(3)
 })
 
 four.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(4)
 })
 five.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(5)
 })
 six.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(6)
 })
 
 seven.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(7)
 })
 
 eight.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(8)
 })
 
 nine.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    insert(9)
+})
+
+zero.addEventListener('click', () => {
+    insert(0)
 })
 
 plus.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    operation('+')
 })
 
 minus.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+    operation('-')
 })
 
-one.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+product.addEventListener('click', () => {
+    operation('x')
 })
 
-one.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+division.addEventListener('click', () => {
+    operation('/')
 })
 
-one.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+operator.addEventListener('click', () => {
+    let finalResult = operate(num1.join(''), num2.join(''), sign)
+    result.innerHTML = finalResult
 })
 
-one.addEventListener('click', () => {
-    if(sign == ''){
-        num1.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }else{
-        num2.push('1')
-        result.innerHTML = `${num1.join('')} ${operator} ${num2.join('')}`
-    }
+clear.addEventListener('click', () => {
+    result.innerHTML = ''
+    num1 = []
+    num2 = []
+    sign = ''
 })
+
+
 
 
